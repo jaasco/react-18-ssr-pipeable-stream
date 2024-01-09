3512baf744340b6f074218d6ece43be7424b09f9
Mikhail Zhukov <mzhukov@stages.team>
2024-01-09 18:41:00 +0400

    add files

---
+++ bin/postbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./.amplify-hosting
+
+mkdir -p ./.amplify-hosting/compute/default
+
+cp ./Server.js ./.amplify-hosting/compute/default/Server.js
+cp ./Bootstrap.js ./.amplify-hosting/compute/default/Bootstrap.js
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+cp -r ./ssr-client/build/static ./.amplify-hosting/compute/default/ssr-client/build/static
+
+cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
