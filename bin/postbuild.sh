6650498dd89cc68b2ea679cce7184ff93ebabec0
Mikhail Zhukov <mzhukov@stages.team>
2024-01-12 13:55:44 +0400

    remove node_modules from build

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -6,7 +6,7 @@ mkdir -p ./.amplify-hosting/compute/default/ssr-client/build/static
 
 cp ./Server.js ./.amplify-hosting/compute/default/Server.js
 cp ./Bootstrap.js ./.amplify-hosting/compute/default/Bootstrap.js
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+# cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r ./ssr-client/build/static ./.amplify-hosting/compute/default/ssr-client/build/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
